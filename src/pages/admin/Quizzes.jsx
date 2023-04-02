f745a95451e9440b9c9989f3f2b0313236e8730f
AzizulHoque <azizulh8774@gmail.com>
2023-04-02 20:27:53 +0600

    quize added

--- src/pages/admin/Quizzes.jsx
+++ src/pages/admin/Quizzes.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import AdminNav from '../../components/AdminNav'
+import React from 'react';
+import AdminNav from '../../components/AdminNav';
 import QuizList from "../../components/admin/quizzes/QuizList";
 
-import {useNavigate} from "react-router-dom";
+import { useState } from "react";
 import Modal from 'react-modal';
-import {useState} from "react";
-import {useDispatch} from "react-redux";
-import {useQuizzeAddMutation} from "../../features/quizzes/quizzesApi";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useQuizzeAddMutation } from "../../features/quizzes/quizzesApi";
 
 const customStyles = {
     content: {
@@ -42,28 +42,41 @@ export default function Quizzes() {
     const [question,setQuestion] = useState("");
     const [video_id,setVideo_id] = useState(null);
     const [video_title,setVideo_title] = useState("");
-    const  [mcq,setMcq] = useState({
-        question:"",
-        video_id:"",
-        video_title:"",
-        options:[
-            {},
-            {},
-            {},
-            {}
-        ],
-    });
-    const handleOptionChange = (e,i)=>{
-        const options = [...mcq.options];
-        options[i] = e.target.value;
-        setMcq({...mcq,options});
-    }
-    const handleAnswerChange = (e)=>{
-        setMcq({...mcq});
-    }
-    const handleSubmit = (e) => {
+    const [option1,setOption1] = useState("");
+    const [option2,setOption2] = useState("");
+    const [option3,setOption3] = useState("");
+    const [option4,setOption4] = useState("");
+    
+    const [isCorrect1,setIsCorrect1] = useState(false);
+    const [isCorrect2,setIsCorrect2] = useState(false);
+    const [isCorrect3,setIsCorrect3] = useState(false);
+    const [isCorrect4,setIsCorrect4] = useState(false);
+   
+    // const  [mcq,setMcq] = useState({
+    //     question:question,
+    //     video_id:video_id,
+    //     video_title:video_title,
+    //     options:[
+    //         {id:1,option:option1,isCorrect:isCorrect1},
+    //         {id:2,option:option2,isCorrect:isCorrect2},
+    //         {id:3,option:option3,isCorrect:isCorrect3},
+    //         {id:4,option:option4,isCorrect:isCorrect4},
+    //     ],
+    // });
+
+    const handleSubmit = (e) => { 
         e.preventDefault();
-        quizzeAdd({question,video_id,video_title});
+        // console.log({question,video_id,video_title,options:[ 
+        //     {id:1,option:option1,isCorrect:isCorrect1},
+        //     {id:2,option:option2,isCorrect:isCorrect2},
+        //     {id:3,option:option3,isCorrect:isCorrect3},
+        //     {id:4,option:option4,isCorrect:isCorrect4},]});
+        // return;
+        quizzeAdd({question,video_id,video_title,options:[ 
+            {id:1,option:option1,isCorrect:isCorrect1},
+            {id:2,option:option2,isCorrect:isCorrect2},
+            {id:3,option:option3,isCorrect:isCorrect3},
+            {id:4,option:option4,isCorrect:isCorrect4},]});
         setQuestion("");
         setVideo_id(null);
         setVideo_title("");
@@ -108,15 +121,68 @@ export default function Quizzes() {
                                    className="login-input "  value={video_id}
                                    onChange={(e) => setVideo_id(e.target.value)}/>
                         </div> <br/>
-                        {mcq.options.map((option,i)=>(
                             <div>
-                                <label htmlFor="option" className="">Option {++i}</label>
-                                <input id="option" name="option" type="text" autoComplete="option" required
-                                       className="login-input " placeholder="Enter option Name" />
-                                <br/>
+                                <label htmlFor="option1" className="">Option 1</label>
+                                <input id="option1" name="option1" type="text" autoComplete="option1" required
+                                       className="login-input " placeholder="Enter option1 Name" type="text"
+                                       value={option1}
+                                       onChange={(e) => setOption1(e.target.value)}
+                                       required/>
+                                    <label htmlFor="isCorrect1" className="">Is Correct 1</label>
+                                  <select name='isCorrect1' value={isCorrect1}
+                                       onChange={(e) => setIsCorrect1(e.target.value)}
+                                       required className="login-input ">
+                                    <option value={false}>False</option>
+                                    <option value={true}>True</option>
+                                </select>
                             </div>
-
-                            ))}
+                            <br/>
+                            <div>
+                                <label htmlFor="option2" className="">Option 2</label>
+                                <input id="option2" name="option2" type="text" autoComplete="option2" required
+                                       className="login-input " placeholder="Enter option 2 Name" type="text"
+                                       value={option2}
+                                       onChange={(e) => setOption2(e.target.value)}
+                                       required/>
+                                    <label htmlFor="isCorrect2" className="">Is Correct 2</label>
+                                  <select name='isCorrect2' value={isCorrect2}
+                                       onChange={(e) => setIsCorrect2(e.target.value)}
+                                       required className="login-input ">
+                                    <option value={false}>False</option>
+                                    <option value={true}>True</option>
+                                </select>
+                            </div>
+                            <br/> <div>
+                                <label htmlFor="option3" className="">Option 3</label>
+                                <input id="option3" name="option3" type="text" autoComplete="option3" required
+                                       className="login-input " placeholder="Enter option Name" type="text"
+                                       value={option3}
+                                       onChange={(e) => setOption3(e.target.value)}
+                                       required/>
+                                    <label htmlFor="isCorrect3" className="">Is Correct 3</label>
+                                  <select name='isCorrect3' value={isCorrect3}
+                                       onChange={(e) => setIsCorrect3(e.target.value)}
+                                       required className="login-input ">
+                                    <option value={false}>False</option>
+                                    <option value={true}>True</option>
+                                </select>
+                            </div>
+                            <br/> <div>
+                                <label htmlFor="option4" className="">Option 4</label>
+                                <input id="option4" n4ame="option" type="text" autoComplete="option4" required
+                                       className="login-input " placeholder="Enter option Name" type="text"
+                                       value={option4}
+                                       onChange={(e) => setOption4(e.target.value)}
+                                       required/>
+                                    <label htmlFor="isCorrect4" className="">Is Correct Option 4</label>
+                                  <select name='isCorrect4' value={isCorrect4}
+                                       onChange={(e) => setIsCorrect4(e.target.value)}
+                                       required className="login-input ">
+                                    <option value={false}>False</option>
+                                    <option value={true}>True</option>
+                                </select>
+                            </div>
+                            <br/>
                     </div>
                     <button type="submit"
                             className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500">
@@ -126,7 +192,6 @@ export default function Quizzes() {
 
                     </div>
                 </form>
-                {/*</div>*/}
             </Modal>
 <AdminNav/>
     <section className="py-6 bg-primary">
