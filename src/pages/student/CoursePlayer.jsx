9686cd802bd5b535ea93dac54c92b1ee5a586f74
mt-azizul <a5@marsanetwork.com>
2023-04-03 12:18:20 +0600

    videos fetched in course player

--- src/pages/student/CoursePlayer.jsx
+++ src/pages/student/CoursePlayer.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Nav from '../../components/Nav';
+import {useGetVideosQuery} from "../../features/videos/videosApi";
 export default function CoursePlayer() {
-  return (
+    const {data,isLoading, isError, error} = useGetVideosQuery();
+
+    return (
     <>
 <Nav/>
     <section className="py-6 bg-primary">
@@ -49,6 +52,8 @@ export default function CoursePlayer() {
 
                     </div>
                 </div>
+
+                {data?.map((video)=>(
                 <div
                     className="col-span-full lg:col-auto max-h-[570px] overflow-y-auto bg-secondary p-4 rounded-md border border-slate-50/10 divide-y divide-slate-600/30">
 
@@ -61,65 +66,24 @@ export default function CoursePlayer() {
                                 d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
                         </svg>
                         {/* <!-- Description --> */}
-                        <div clas="flex flex-col w-full">
-                            <a href="#">
-                                <p className="text-slate-50 text-sm font-medium">Things I wish I knew as a Junior Web
-                                    Developer - Sumit Saha - BASIS SoftExpo 2023</p>
-                            </a>
-                            <div>
-                                <span className="text-gray-400 text-xs mt-1">34.5 Mins</span>
-                                <span className="text-gray-400 text-xs mt-1"> | </span>
-                                <span className="text-gray-400 text-xs mt-1">241K views</span>
-                            </div>
-                        </div>
-                    </div>
 
-                    <div className="w-full flex flex-row gap-2 cursor-pointer hover:bg-slate-900 p-2">
-                        {/* <!-- Thumbnail --> */}
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" className="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
-                        </svg>
-                        {/* <!-- Description --> */}
-                        <div clas="flex flex-col w-full">
-                            <a href="#">
-                                <p className="text-slate-50 text-sm font-medium">Introduction to Couse</p>
-                            </a>
-                            <div>
-                                <span className="text-gray-400 text-xs mt-1">34.5 Mins</span>
-                                <span className="text-gray-400 text-xs mt-1"> | </span>
-                                <span className="text-gray-400 text-xs mt-1">241K views</span>
-                            </div>
-                        </div>
-                    </div>
 
-                    <div className="w-full flex flex-row gap-2 cursor-pointer hover:bg-slate-900 p-2">
-                        {/* <!-- Thumbnail --> */}
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" className="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
-                        </svg>
-                        {/* <!-- Description --> */}
+
                         <div clas="flex flex-col w-full">
                             <a href="#">
-                                <p className="text-slate-50 text-sm font-medium">Introduction to Couse</p>
+                                <p className="text-slate-50 text-sm font-medium">{video.title}</p>
                             </a>
                             <div>
-                                <span className="text-gray-400 text-xs mt-1">34.5 Mins</span>
+                                <span className="text-gray-400 text-xs mt-1">{video.duration}</span>
                                 <span className="text-gray-400 text-xs mt-1"> | </span>
-                                <span className="text-gray-400 text-xs mt-1">241K views</span>
+                                <span className="text-gray-400 text-xs mt-1">{video.views} views</span>
                             </div>
                         </div>
                     </div>
 
                 </div>
-            </div>
+                ))}
+                </div>
         </div>
     </section>
     </>
